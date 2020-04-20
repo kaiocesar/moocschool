ad833a3deb09efdb2828ce7678bb27e41020b452
Kaio Cesar <tecnico.kaio@gmail.com>
2020-04-19 22:05:30 -0300

    rota de criação de curso

--- app/controllers/api/v1/courses_controller.rb
+++ app/controllers/api/v1/courses_controller.rb
@@ -2,4 +2,12 @@ class Api::V1::CoursesController < ApplicationController
     def index
         @courses = Course.all
     end
+
+    def create
+        head :ok
+    end
+
+    def update
+    end
+
 end
