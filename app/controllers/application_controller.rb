6b4edc60a1c588722a78decae77c6ae568e7a59c
Kaio Cesar <tecnico.kaio@gmail.com>
2020-04-25 21:04:35 -0300

    tentativa de fix no docker

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,3 @@
 class ApplicationController < ActionController::API
-  # include ActionController::RequestForgeryProtection
   before_action :authenticate_user!
-  
-  # protect_from_forgery with: :null_session
-  # protect_from_forgery unless: -> { request.format.json? }
 end
--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,6 @@ module Moocschool
   class Application < Rails::Application
     config.api_only = true
     config.load_defaults 6.0
-    config.middleware.use ActionDispatch::Flash
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
